78bb0505ead92849ab7711bf30bd3ad06d949557
Nathan Eloe <powerofazure@gmail.com>
2021-02-23 09:18:22 -0600

    Add parallax background

---
+++ Level1.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+
+
+func _on_Area2D_body_entered(body):
+	get_tree().change_scene("res://Level1.tscn")
--- Player.gd
+++ Player.gd
@@ -30,3 +30,4 @@ func _physics_process(delta):
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	velocity.x = lerp(velocity.x, 0, 0.2)
+
